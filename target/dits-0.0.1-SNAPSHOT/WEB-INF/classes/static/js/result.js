74f516a26f2af4c40e42a591c38f4f945c23d023
Alexey <katomachin@mail.ru>
2022-01-24 12:25:48 +0300

    add gitignore and update

--- target/dits-0.0.1-SNAPSHOT/WEB-INF/classes/static/js/result.js
+++
@@ -1,5 +0,0 @@
-
-var percentageComplete = 0.7;
-var strokeDashOffsetValue = 100 - (percentageComplete * 100);
-var progressBar = $(".js-progress-bar");
-progressBar.css("stroke-dashoffset", strokeDashOffsetValue);
