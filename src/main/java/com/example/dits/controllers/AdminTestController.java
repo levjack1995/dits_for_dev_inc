4ae8a85706dc2f4c88d01819f13a4e5fd2e571c2
KarasikKk <bayanist1995@mail.ru>
2022-02-06 16:21:45 +0300

    minor

--- src/main/java/com/example/dits/controllers/AdminTestController.java
+++ src/main/java/com/example/dits/controllers/AdminTestController.java
@@ -9,14 +9,13 @@ import com.example.dits.entity.Topic;
 import com.example.dits.entity.User;
 import com.example.dits.service.TestService;
 import com.example.dits.service.TopicService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -24,46 +23,45 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/testBuilder")
 public class AdminTestController {
     private final ModelMapper modelMapper;
     private final TopicService topicService;
     private final TestService testService;
+    private final ObjectMapper mapper;
 
-    @GetMapping("")
+    @GetMapping("/testBuilder")
     public String getTopics(ModelMap model) {
         List<Topic> topicList = topicService.findAll();
         List<TopicDTO> topicDTOList = topicList.stream().map(this::convertToDTO).collect(Collectors.toList());
         model.addAttribute("topicList",topicDTOList);
-        return "admin/admin4";
+        return "admin/index";
     }
 
-    @PostMapping("")
-    public List<TestWithQuestionsDTO> getTestsWithQuestions(@RequestParam int ID,ModelMap modelMap) {
-        List<Test> testList = testService.getTestsByTopic_TopicId(ID);
-        List<TestWithQuestionsDTO> testWithQuestionsDTOList = testList.stream().map(this::convertToDTO).collect(Collectors.toList());
-        return testWithQuestionsDTOList;
+    @GetMapping("/getTests")
+    public List<TestWithQuestionsDTO> getTestsWithQuestions(@RequestParam int id) {
+        List<Test> testList = testService.getTestsByTopic_TopicId(id);
+        return testList.stream().map(this::convertToDTO).collect(Collectors.toList());
     }
 
     @GetMapping("/removeTest")
     public String removeTest(@RequestParam int testId){
         Test test = testService.getTestByTestId(testId);
         testService.delete(test);
-        return "redirect:/admin4";
+        return "redirect:/admin/index";
     }
 
     @GetMapping("/removeTopic")
     public String removeTopic(@RequestParam String topicId){
         Topic topic = topicService.getTopicByName(topicId);
         topicService.delete(topic);
-        return "redirect:/admin4";
+        return "redirect:/admin/index";
     }
 
     @GetMapping("/addTopic")
     public String removeUser(@RequestParam String name,@RequestParam String description){
         Topic topic = new Topic(name,description);
         topicService.save(topic);
-        return "redirect:/admin4";
+        return "redirect:/admin/index";
     }
 
 
