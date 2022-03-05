5aeec36d879388c4db5a43d2d86290c2af2cf26b
Artiom Sitko <artiom.sitko@gmail.com>
2022-03-05 23:34:00 +0300

    Test requires one answer selected at least
    One selected answer required at least when passing each test.

---
+++ src/main/resources/static/js/test-item.js
@@ -0,0 +1,24 @@
+const formTestItem = document.getElementById("form-test-item");
+
+const buttonSubmit = formTestItem.querySelector("button[type=\"submit\"]");
+const testAnswers = formTestItem.querySelectorAll(".answer_variant > input[type=\"checkbox\"]");
+
+if (buttonSubmit !== null) {
+    buttonSubmit.addEventListener("click", e => {
+        e.preventDefault();
+    
+        if (testAnswers.length > 0 && isAnyAnswerSelected(testAnswers)) {
+            formTestItem.submit();
+        }
+    });
+}
+
+function isAnyAnswerSelected(checkboxList) {
+    for (let i = 0; i < checkboxList.length; i++) {
+        if (checkboxList[i].checked) {
+            return true;
+        }
+    }
+
+    return false;
+}
