3f1ea76c552a957db176a848c7781e26d6a4973f
Artiom Sitko <artiom.sitko@gmail.com>
2022-03-06 15:07:57 +0300

    Add TODO section in result.js

--- src/main/resources/static/js/result.js
+++ src/main/resources/static/js/result.js
@@ -1,4 +1,8 @@
-
+/**
+ * TODO:
+ * 
+ * - Check if this file is needed and delete if not
+ */
 var percentageComplete = 0.7;
 var strokeDashOffsetValue = 100 - (percentageComplete * 100);
 var progressBar = $(".js-progress-bar");
